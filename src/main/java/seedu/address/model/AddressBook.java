7817331d258e07c4aafb031edcee67841b88306f
Barnabas Sng <barnabassng10@gmail.com>
2026-03-21 00:10:40 +0800

    Add support for matches in AddressBook

--- src/main/java/seedu/address/model/AddressBook.java
+++ src/main/java/seedu/address/model/AddressBook.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 import javafx.collections.ObservableList;
 import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.model.match.Match;
+import seedu.address.model.match.UniqueMatchList;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.UniquePersonList;
 
@@ -16,6 +18,7 @@ import seedu.address.model.person.UniquePersonList;
 public class AddressBook implements ReadOnlyAddressBook {
 
     private final UniquePersonList persons;
+    private final UniqueMatchList matches;
 
     /*
      * The 'unusual' code block below is a non-static initialization block, sometimes used to avoid duplication
@@ -26,12 +29,13 @@ public class AddressBook implements ReadOnlyAddressBook {
      */
     {
         persons = new UniquePersonList();
+        matches = new UniqueMatchList();
     }
 
     public AddressBook() {}
 
     /**
-     * Creates an AddressBook using the Persons in the {@code toBeCopied}
+     * Creates an AddressBook using the Persons and Matches in the {@code toBeCopied}
      */
     public AddressBook(ReadOnlyAddressBook toBeCopied) {
         this();
@@ -49,12 +53,21 @@ public class AddressBook implements ReadOnlyAddressBook {
     }
 
     /**
+     * Replaces the contents of the match list with {@code matches}.
+     * {@code matches} must not contain duplicate matches.
+     */
+    public void setMatches(List<Match> matches) {
+        this.matches.setMatches(matches);
+    }
+
+    /**
      * Resets the existing data of this {@code AddressBook} with {@code newData}.
      */
     public void resetData(ReadOnlyAddressBook newData) {
         requireNonNull(newData);
 
         setPersons(newData.getPersonList());
+        setMatches(newData.getMatchList());
     }
 
     //// person-level operations
@@ -94,18 +107,53 @@ public class AddressBook implements ReadOnlyAddressBook {
         persons.remove(key);
     }
 
+    //// match-level operations
+
+    /**
+     * Returns true if a match with the same identity as {@code match} exists in the address book.
+     */
+    public boolean hasMatch(Match match) {
+        requireNonNull(match);
+        return matches.contains(match);
+    }
+
+    /**
+     * Adds a match to the address book.
+     * The match must not already exist in the address book.
+     */
+    public void addMatch(Match m) {
+        matches.add(m);
+    }
+
+    /**
+     * Replaces the given match {@code target} in the list with {@code editedMatch}.
+     * {@code target} must exist in the address book.
+     * The match identity of {@code editedMatch} must not be the same as another existing match in the address book.
+     */
+    public void setMatch(Match target, Match editedMatch) {
+        requireNonNull(editedMatch);
+
+        matches.setMatch(target, editedMatch);
+    }
+
     //// util methods
 
     @Override
     public String toString() {
         return new ToStringBuilder(this)
                 .add("persons", persons)
+                .add("matches", matches)
                 .toString();
     }
 
     @Override
     public ObservableList<Person> getPersonList() {
         return persons.asUnmodifiableObservableList();
+    }
+
+    @Override
+    public ObservableList<Match> getMatchList() {
+        return matches.asUnmodifiableObservableList();
     }
 
     @Override
