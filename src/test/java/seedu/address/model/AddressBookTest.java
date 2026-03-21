c550f29a61dcfcbf034167fd717e48bc86651611
Barnabas Sng <barnabassng10@gmail.com>
2026-03-21 11:52:43 +0800

    Add matches to AdressBookStub

--- src/test/java/seedu/address/model/AddressBookTest.java
+++ src/test/java/seedu/address/model/AddressBookTest.java
@@ -18,6 +18,7 @@ import org.junit.jupiter.api.Test;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import seedu.address.model.match.Match;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.exceptions.DuplicatePersonException;
 import seedu.address.testutil.PersonBuilder;
@@ -96,6 +97,7 @@ public class AddressBookTest {
      */
     private static class AddressBookStub implements ReadOnlyAddressBook {
         private final ObservableList<Person> persons = FXCollections.observableArrayList();
+        private final ObservableList<Match> matches = FXCollections.observableArrayList();
 
         AddressBookStub(Collection<Person> persons) {
             this.persons.setAll(persons);
@@ -105,6 +107,8 @@ public class AddressBookTest {
         public ObservableList<Person> getPersonList() {
             return persons;
         }
+        public ObservableList<Match> getMatchList() {
+            return matches;
+        }
     }
-
 }
