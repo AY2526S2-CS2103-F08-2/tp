ab4669348213637d40c4399b0b7ee457034212fa
Ng Wei Zong Damian <damianng87@gmail.com>
2026-03-23 23:34:58 +0800

    feat(find): support role-filtered search

--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -3,15 +3,22 @@ package seedu.address.logic.parser;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
 
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
+import seedu.address.model.person.Role;
+import seedu.address.model.person.RoleFilteredNameContainsKeywordsPredicate;
 
 /**
  * Parses input arguments and creates a new FindCommand object
  */
 public class FindCommandParser implements Parser<FindCommand> {
+    private static final String ROLE_PLAYER = "player";
+    private static final String ROLE_PLAYERS = "players";
+    private static final String ROLE_STAFF = "staff";
 
     /**
      * Parses the given {@code String} of arguments in the context of the FindCommand
@@ -25,9 +32,33 @@ public class FindCommandParser implements Parser<FindCommand> {
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
         }
 
-        String[] nameKeywords = trimmedArgs.split("\\s+");
+        String[] tokens = trimmedArgs.split("\\s+");
+        String firstToken = tokens[0].toLowerCase(Locale.ROOT);
+
+        if (isRoleToken(firstToken)) {
+            if (tokens.length == 1) {
+                throw new ParseException(
+                        String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+            }
+            Role role = parseRole(firstToken);
+            List<String> nameKeywords = Arrays.asList(Arrays.copyOfRange(tokens, 1, tokens.length));
+            return new FindCommand(new RoleFilteredNameContainsKeywordsPredicate(role, nameKeywords));
+        }
+
+        String[] nameKeywords = tokens;
 
         return new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+    }
+
+    private boolean isRoleToken(String token) {
+        return ROLE_PLAYER.equals(token) || ROLE_PLAYERS.equals(token) || ROLE_STAFF.equals(token);
+    }
+
+    private Role parseRole(String token) {
+        if (ROLE_STAFF.equals(token)) {
+            return Role.STAFF;
+        }
+        return Role.PLAYER;
     }
 
 }
---
+++ src/main/java/seedu/address/model/person/RoleFilteredNameContainsKeywordsPredicate.java
@@ -0,0 +1,52 @@
+package seedu.address.model.person;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+import seedu.address.commons.util.ToStringBuilder;
+
+/**
+ * Tests that a {@code Person} has the target role and name matches any of the keywords given.
+ */
+public class RoleFilteredNameContainsKeywordsPredicate implements Predicate<Person> {
+    private final PersonHasRolePredicate rolePredicate;
+    private final NameContainsKeywordsPredicate namePredicate;
+
+    /**
+     * Creates a predicate that matches persons with the given {@code role} whose names
+     * contain at least one of the given {@code keywords}.
+     */
+    public RoleFilteredNameContainsKeywordsPredicate(Role role, List<String> keywords) {
+        this.rolePredicate = new PersonHasRolePredicate(role);
+        this.namePredicate = new NameContainsKeywordsPredicate(keywords);
+    }
+
+    @Override
+    public boolean test(Person person) {
+        return rolePredicate.test(person) && namePredicate.test(person);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof RoleFilteredNameContainsKeywordsPredicate)) {
+            return false;
+        }
+
+        RoleFilteredNameContainsKeywordsPredicate otherPredicate = (RoleFilteredNameContainsKeywordsPredicate) other;
+        return rolePredicate.equals(otherPredicate.rolePredicate)
+                && namePredicate.equals(otherPredicate.namePredicate);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("rolePredicate", rolePredicate)
+                .add("namePredicate", namePredicate)
+                .toString();
+    }
+}
