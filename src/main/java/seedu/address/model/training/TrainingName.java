1ea4f2be34cd4c72067daf41784ae879f080c9dc
RS <69071162+Arrus218@users.noreply.github.com>
2026-03-24 18:36:18 +0800

    Create Training package and add relevant classes

---
+++ src/main/java/seedu/address/model/training/TrainingName.java
@@ -0,0 +1,66 @@
+package seedu.address.model.training;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents a Training's opponent name in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidName(String)}
+ */
+public class TrainingName {
+
+    public static final String MESSAGE_CONSTRAINTS =
+            "Opponent team names should only contain alphanumeric characters and spaces, and it should not be blank";
+
+    /*
+     * The first character of the name must not be a whitespace,
+     * otherwise " " (a blank string) becomes a valid input.
+     */
+    public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
+
+    public final String fullName;
+
+    /**
+     * Constructs an {@code OpponentName}.
+     *
+     * @param opponentName A valid name.
+     */
+    public OpponentName(String opponentName) {
+        requireNonNull(opponentName);
+        checkArgument(isValidName(opponentName), MESSAGE_CONSTRAINTS);
+        fullName = opponentName;
+    }
+
+    /**
+     * Returns true if a given string is a valid name.
+     */
+    public static boolean isValidName(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+
+    @Override
+    public String toString() {
+        return fullName;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof OpponentName)) {
+            return false;
+        }
+
+        OpponentName otherName = (OpponentName) other;
+        return fullName.equals(otherName.fullName);
+    }
+
+    @Override
+    public int hashCode() {
+        return fullName.hashCode();
+    }
+}
