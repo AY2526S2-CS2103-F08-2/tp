4cae47b4817053f98ab5e25236fc67f19e41bebd
RS <69071162+Arrus218@users.noreply.github.com>
2026-03-24 18:36:18 +0800

    Create Training package and add relevant classes

---
+++ src/main/java/seedu/address/model/training/exceptions/TrainingNotFoundException.java
@@ -0,0 +1,6 @@
+package seedu.address.model.training.exceptions;
+
+/**
+ * Signals that the operation is unable to find the specified training.
+ */
+public class TrainingNotFoundException extends RuntimeException {}
