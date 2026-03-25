d57ceaf0a573798a9908f2104c8b09e5e0188ead
Neo Ryan <63915587+ryrybn@users.noreply.github.com>
2026-03-26 00:17:32 +0800

    style(model): fix CustomImportOrder in ModelManager imports

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -4,8 +4,8 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
 import java.nio.file.Path;
-import java.util.List;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.function.Predicate;
 import java.util.logging.Logger;
