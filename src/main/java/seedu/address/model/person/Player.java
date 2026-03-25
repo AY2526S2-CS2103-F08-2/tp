9a5bb4d5cf77f1f880e37cf8cce6fa9caad3506f
zenercurrent <11849012+zenercurrent@users.noreply.github.com>
2026-03-25 16:49:42 +0800

    Create enum StatField to abstract player stats

--- src/main/java/seedu/address/model/person/Player.java
+++ src/main/java/seedu/address/model/person/Player.java
@@ -11,6 +11,15 @@ public class Player extends Person {
 
     private PlayerStats stats;
 
+    /**
+     * Constructs a {@code Player} with the given data.
+     *
+     * @param name the player name
+     * @param phone the player phone number
+     * @param email the player email
+     * @param address the player address
+     * @param tags the player tags
+     */
     public Player(Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
         super(name, phone, email, address, tags, Role.PLAYER);
     }
