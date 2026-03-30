09d31244e55c3ac9c12426a3ac0e1e3c69d0e331
Ng Wei Zong Damian <damianng87@gmail.com>
2026-03-30 21:11:33 +0800

    feat(attributes): recover gracefully from Json missing default attributes

--- src/test/java/seedu/address/storage/JsonSerializableAddressBookTest.java
+++ src/test/java/seedu/address/storage/JsonSerializableAddressBookTest.java
@@ -1,6 +1,7 @@
 package seedu.address.storage;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import java.nio.file.Path;
@@ -11,6 +12,9 @@ import org.junit.jupiter.api.Test;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.commons.util.JsonUtil;
 import seedu.address.model.AddressBook;
+import seedu.address.model.person.Position;
+import seedu.address.model.person.Status;
+import seedu.address.model.person.Team;
 import seedu.address.testutil.TypicalPersons;
 
 public class JsonSerializableAddressBookTest {
@@ -22,6 +26,8 @@ public class JsonSerializableAddressBookTest {
     private static final Path DUPLICATE_TEAM_FILE = TEST_DATA_FOLDER.resolve("duplicateTeamAddressBook.json");
     private static final Path DUPLICATE_POSITION_FILE = TEST_DATA_FOLDER.resolve("duplicatePositionAddressBook.json");
     private static final Path DUPLICATE_STATUS_FILE = TEST_DATA_FOLDER.resolve("duplicateStatusAddressBook.json");
+    private static final Path MISSING_DEFAULT_ATTRIBUTES_FILE =
+            TEST_DATA_FOLDER.resolve("missingDefaultAttributesAddressBook.json");
 
     @Test
     public void toModelType_typicalPersonsFile_success() throws Exception {
@@ -69,6 +75,20 @@ public class JsonSerializableAddressBookTest {
                 JsonSerializableAddressBook.class).get();
         assertThrows(IllegalValueException.class, JsonSerializableAddressBook.MESSAGE_DUPLICATE_STATUS,
                 dataFromFile::toModelType);
+    }
+
+    @Test
+    public void toModelType_missingDefaultAttributes_defaultsAreAutoAdded() throws Exception {
+        JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(MISSING_DEFAULT_ATTRIBUTES_FILE,
+                JsonSerializableAddressBook.class).get();
+        AddressBook addressBookFromFile = dataFromFile.toModelType();
+
+        assertTrue(addressBookFromFile.hasTeam(new Team(Team.DEFAULT_UNASSIGNED_TEAM)));
+        assertTrue(addressBookFromFile.hasPosition(new Position(Position.DEFAULT_UNASSIGNED_POSITION)));
+        assertTrue(addressBookFromFile.hasStatus(new Status(Status.DEFAULT_UNKNOWN_STATUS)));
+        assertEquals(new Team(Team.DEFAULT_UNASSIGNED_TEAM), addressBookFromFile.getTeamList().get(0));
+        assertEquals(new Position(Position.DEFAULT_UNASSIGNED_POSITION), addressBookFromFile.getPositionList().get(0));
+        assertEquals(new Status(Status.DEFAULT_UNKNOWN_STATUS), addressBookFromFile.getStatusList().get(0));
     }
 
 }
